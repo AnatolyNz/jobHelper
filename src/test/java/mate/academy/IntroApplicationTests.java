c8f1746b9c2c0aa86b961da5c91d14770b38f417
Anatolii Nazarov <anatoly_n@ukr.net>
2025-07-10 18:56:37 +0300

    Initial commit - base from old project

---
+++ src/test/java/mate/academy/IntroApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = IntroApplicationTests.class)
+class IntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
