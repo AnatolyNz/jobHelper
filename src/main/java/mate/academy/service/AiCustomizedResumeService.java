a10589b4cdc4d0e27c83582d5ea69e27d2aa2d59
Anatolii Nazarov <anatoly_n@ukr.net>
2025-11-12 18:28:47 +0200

    Deleted not use classes.

--- src/main/java/mate/academy/service/AiCustomizedResumeService.java
+++
@@ -1,7 +0,0 @@
-package mate.academy.service;
-
-import mate.academy.model.AiCustomizedResume;
-
-public interface AiCustomizedResumeService {
-    AiCustomizedResume generateCustomizedResume(Long resumeId, Long jobId);
-}
