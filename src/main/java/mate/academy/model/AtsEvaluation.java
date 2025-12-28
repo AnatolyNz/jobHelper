4e6eea0640bce97a3a5c93193f79b92430ab5d80
Anatolii Nazarov <anatoly_n@ukr.net>
2025-12-28 12:07:11 +0200

    Fix added uniqueConstraints for "ats_evaluations".

--- src/main/java/mate/academy/model/AtsEvaluation.java
+++ src/main/java/mate/academy/model/AtsEvaluation.java
@@ -7,11 +7,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Data;
 
 @Entity
 @Data
-@Table(name = "ats_evaluations")
+@Table(name = "ats_evaluations",
+        uniqueConstraints = @UniqueConstraint(columnNames = "resume_id"))
 public class AtsEvaluation {
 
     @Id
